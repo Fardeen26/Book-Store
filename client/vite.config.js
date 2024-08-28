70cbb336b4dc2d2489c39b14ab928d7e0e2acb9d
Fardeen Mansoori <137139589+Fardeen26@users.noreply.github.com>
2024-08-28 22:42:35 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://book-store-backend-z5yv.onrender.com'
-    }
-  },
   plugins: [react()],
 })
